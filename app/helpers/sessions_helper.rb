acc044769a87586aebed3bcf212b593657d9fa70
gregbast1994 <gregbast1994@gmail.com>
2019-06-18 15:52:40 -0400

    got up to 8.2.4

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+
+    #logs in the given user
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    #returns current user if logged in
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+
+end
