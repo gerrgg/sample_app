655e8d6344bcb34d20930723383a87ed36db46e9
gregbast1994 <gregbast1994@gmail.com>
2019-06-18 22:03:03 -0400

    implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,13 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      #error message
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -28,7 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_select 'div.alert-success'
+    assert is_logged_in?
   end
 
 
