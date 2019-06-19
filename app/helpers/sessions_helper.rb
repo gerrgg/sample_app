ccf9881ab21d51c919bb822132872d2abc240009
gregbast1994 <gregbast1994@gmail.com>
2019-06-19 16:36:17 -0400

    10.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,16 +13,20 @@ module SessionsHelper
     end
 
     def current_user
-        if (user_id = session[:user_id])
-          @current_user ||= User.find_by(id: user_id)
-        elsif (user_id = cookies.signed[:user_id])
-          user = User.find_by(id: user_id)
-          if user && user.authenticated?(cookies[:remember_token])
-            log_in user
-            @current_user = user
-          end
+      if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
         end
       end
+    end
+
+    def current_user?(user)
+      user == current_user
+    end
 
     #checks whether or not the current_user method returns a user
     def logged_in?
